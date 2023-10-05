83c34fefb8de76213541abbbffca51179c752f78
Rohan Maharjan <nahor.uzamaki@gmail.com>
2023-10-05 22:38:40 +0545

    Update Operators.java

--- Operators.java
+++ Operators.java
@@ -2,7 +2,10 @@
 public class Operators {
     public static void main(String[] args){
         int a = 4;
-        int b = 6 + a;
+        // int b = 6 + a;
+
+        int b = 9;
+        b += 3;
         System.out.println(b);
     }
 
