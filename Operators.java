b70b874b15c27eecdcaf14a231f86a91930a3e75
Rohan Maharjan <nahor.uzamaki@gmail.com>
2023-10-05 22:49:50 +0545

    printed operation results

--- Operators.java
+++ Operators.java
@@ -4,9 +4,13 @@ public class Operators {
         int a = 4;
         // int b = 6 + a;
 
+        // assignment operators
         int b = 9;
         b += 3;
         System.out.println(b);
+        System.out.println(64>6); // Comparison Operators
+        System.out.println(64>5 && 64>8); //Logical Operators
+        System.out.println(2&3); //bit wise operators
     }
 
 }
