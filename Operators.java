a54dfc7960357125fdc142de17d084fb3bd72d26
Rohan Maharjan <nahor.uzamaki@gmail.com>
2023-10-06 23:56:43 +0545

    commit

--- Operators.java
+++ Operators.java
@@ -1,12 +1,13 @@
 
 public class Operators {
     public static void main(String[] args){
-        // int a = 4;
+        int a = 4;
         // int b = 6 + a;
 
         // assignment operators
         int b = 9;
         b += 3;
+        System.out.println(a);
         System.out.println(b);
         System.out.println(64>6); // Comparison Operators
         System.out.println(64>5 && 64>8); //Logical Operators
