b5e386125e2aaff6e7c71fceb86fd98237eafd1b
Rohan Maharjan <nahor.uzamaki@gmail.com>
2023-10-07 23:18:06 +0545

    Update Operators.java

--- Operators.java
+++ Operators.java
@@ -5,10 +5,13 @@ public class Operators {
         // int b = 6 + a;
 
         // assignment operators
-        int b = 9;
+        int b = 9, c;
         b += 3;
         System.out.println(a);
         System.out.println(b);
+        System.out.println(a+b); // Arithmetic Operator
+        c = b - a; //Assignment Operator
+        System.out.println(c);
         System.out.println(64>6); // Comparison Operators
         System.out.println(64>5 && 64>8); //Logical Operators
         System.out.println(2&3); //bit wise operators
