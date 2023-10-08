4119a5c6f82cc1f934d0d17153969561791d6b26
Rohan Maharjan <nahor.uzamaki@gmail.com>
2023-10-08 23:51:18 +0545

    concatenation of strings

---
+++ StringOperations.java
@@ -0,0 +1,11 @@
+public class StringOperations {
+    public static void main(String[] args) {
+        // Declare and initialize strings
+        String str1 = "Hello, ";
+        String str2 = "World!";
+        
+        // Concatenation of strings
+        String result = str1 + str2;
+        System.out.println("Concatenated String: " + result);
+    }
+}
