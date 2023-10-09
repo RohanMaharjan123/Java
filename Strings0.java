002ccaf27bbe6b1f7ef635491bc4302f2eb4c8c2
Rohan Maharjan <nahor.uzamaki@gmail.com>
2023-10-09 18:49:29 +0545

    printing formats

---
+++ Strings0.java
@@ -0,0 +1,12 @@
+public class Strings0{
+    public static void main(String[] args){
+        String name = "ruchi";
+        System.out.print("The name is: ");
+        System.out.println(name);
+        
+        int a = 6;
+        float b = 5.6454f;
+        System.out.printf("The value of a is %d and value of b is %f", a, b);
+        System.out.format("The value of a is %d and value of b is %f", a, b);
+    }
+}
