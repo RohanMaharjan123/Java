91263c4c457d8dbfb61db47a5d127b7b58bef81e
Rohan Maharjan <nahor.uzamaki@gmail.com>
2023-10-09 19:00:04 +0545

    tiny change

--- Strings0.java
+++ Strings0.java
@@ -6,7 +6,9 @@ public class Strings0{
         
         int a = 6;
         float b = 5.6454f;
-        System.out.printf("The value of a is %d and value of b is %f", a, b);
-        System.out.format("The value of a is %d and value of b is %f", a, b);
+        // System.out.printf("The value of a is %d and value of b is %f", a, b);
+        System.out.printf("The value of a is %d and value of b is %8.2f", a, b);
+        // System.out.format("The value of a is %d and value of b is %f", a, b);
+        System.out.format("The value of a is %d and value of b is %.2f", a, b);
     }
 }
