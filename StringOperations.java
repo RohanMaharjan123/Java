de0d520035e83a90c21c7372ac1aba5f7eb22d65
Rohan Maharjan <nahor.uzamaki@gmail.com>
2023-10-09 19:06:17 +0545

    String methods

--- StringOperations.java
+++ StringOperations.java
@@ -7,5 +7,27 @@ public class StringOperations {
         // Concatenation of strings
         String result = str1 + str2;
         System.out.println("Concatenated String: " + result);
+             // Length of a string
+            int length = result.length();
+            System.out.println("Length of the String: " + length);
+            
+             // Extracting substring
+            String substring = result.substring(0, 5);
+            System.out.println("Substring: " + substring);
+            
+             // Uppercase and lowercase conversion
+            String uppercase = result.toUpperCase();
+            String lowercase = result.toLowerCase();
+            System.out.println("Uppercase: " + uppercase);
+            System.out.println("Lowercase: " + lowercase);
+            
+             // Checking if a string contains a specific substring
+            boolean contains = result.contains("Hello");
+            System.out.println("Contains 'Hello': " + contains);
+            
+             // Checking if two strings are equal
+            String anotherString = "Hello, World!";
+            boolean isEqual = result.equals(anotherString);
+            System.out.println("Is equal to 'Hello, World!': " + isEqual);
     }
 }
---
+++ Stringmethods.java
@@ -0,0 +1,9 @@
+public class Stringmethods {
+    public static void main(String[] args){
+        String a = "ruchi";
+        System.out.println(a);
+        String a1 = new String("ruchi");
+        System.out.println(a1);
+
+    }
+}
