2f1691d1072f8aaf3d4de602f4a448704d74eb59
Rohan Maharjan <nahor.uzamaki@gmail.com>
2023-10-10 20:57:40 +0545

    Delete Strings1.java

--- Strings1.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class Strings1 {
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        // String st = sc.next();
-        String st = sc.nextLine();
-        System.out.println(st);
-    }
-}
