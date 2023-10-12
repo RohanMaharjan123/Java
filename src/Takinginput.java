9989ab9cebfe8e77be0d85936988471201d82bd0
RohanMaharjan123 <nahor.uzamaki@gmail.com>
2023-10-12 19:39:20 +0545

    small changes

--- src/Takinginput.java
+++ src/Takinginput.java
@@ -4,10 +4,14 @@ public class Takinginput {
         System.out.println("Taking Input From the User");
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter number 1");
-        int a = sc.nextInt();
+        // int a = sc.nextInt();
+        float  a = sc.nextFloat();
         System.out.println("Enter number 2");
-        int b = sc.nextInt();
-        int sum = a + b;
+        // int b = sc.nextInt();
+        float b = sc.nextFloat();
+        // int sum = a + b;
+        float sum = a + b;
+
         System.out.println("The sum of these numbers is:");
         System.out.println(sum);
     }
