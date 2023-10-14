ad3aa74ff9e10c4ed8ce05b0d9f1e858e60274a8
Rohan Maharjan <nahor.uzamaki@gmail.com>
2023-10-14 23:55:04 +0545

    String input

---
+++ src/TakinginputS.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class TakinginputS {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a string:");
+        String userInput = scanner.nextLine();
+        System.out.println("You entered: " + userInput);
+        scanner.close();
+    }
+}
+
+
