392114d2518a3a7746fe490cf554a4ba2580c10a
Rohan Maharjan <nahor.uzamaki@gmail.com>
2023-10-20 00:28:57 +0545

    comments

--- src/Variables.java
+++ src/Variables.java
@@ -2,6 +2,9 @@ public class Variables {
     public static void main(String[] args) {
         //Code 
         System.out.println("Hello World");
-        
+
+        // AddingThreeNumbers --> Pascal Naming Convension
+        // addingThree Numbers --> camel Case Naming Conversion
+
     }
 }
