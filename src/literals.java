0ebca25908d01b0bdfbbda3cfc212f473e3258a3
Rohan Maharjan <nahor.uzamaki@gmail.com>
2023-10-19 23:50:56 +0545

    literals

---
+++ src/literals.java
@@ -0,0 +1,9 @@
+public class literals {
+    public static void main(String[] args){
+        // Literals
+        byte age = 34;
+        char ch = 'A';
+        float f = 5.6f;
+        System.out.println(age);
+    }
+}
