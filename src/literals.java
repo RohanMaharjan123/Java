c05b30aae9ead2da4f7bc5a0276f69fd9c6ed73b
Rohan Maharjan <nahor.uzamaki@gmail.com>
2023-10-19 23:59:19 +0545

    changes

--- src/literals.java
+++ src/literals.java
@@ -1,9 +1,26 @@
 public class literals {
     public static void main(String[] args){
         // Literals
+        int decimal = 100; // Decimal literal
+        int octal = 0144; // Octal literal
+        int hex = 0x64; // Hexadecimal literal
+        int binary = 0b1100100; // Binary literal
+        double d = 123.456; // Double literal
+        float f = 123.456F; // Float literal
+        boolean b = true;
+        String s = "Hello, World!";
         byte age = 34;
         char ch = 'A';
-        float f = 5.6f;
+        System.out.println(decimal);
+        System.out.println(octal);
+        System.out.println(hex);
+        System.out.println(binary);
+        System.out.println(d);
+        System.out.println(f);
+        System.out.println(b);
+        System.out.println(s);
         System.out.println(age);
+        System.out.println(ch);
+
     }
 }
