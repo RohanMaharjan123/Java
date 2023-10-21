d38b42f029681124226d71b79e41e606c080f0bd
Rohan Maharjan <nahor.uzamaki@gmail.com>
2023-10-21 23:58:57 +0545

    if else conditions

---
+++ src/Ch4/ifelse_conditionals.java
@@ -0,0 +1,11 @@
+package Ch4;
+public class ifelse_conditionals{
+    public static void main(String[] args){
+        int age = 9;
+        if (age> 18) {
+            System.out.println("Yes man now you can drive");
+        } else {
+            System.out.println("Not man you cannot drive yet");
+        }
+    }
+}
