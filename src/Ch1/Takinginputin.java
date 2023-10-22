2a8b01f43740b9dff086648b83c657e498bbca2d
Rohan Maharjan <nahor.uzamaki@gmail.com>
2023-10-22 19:57:48 +0545

    fixes in function

--- src/Ch1/Takinginputin.java
+++ src/Ch1/Takinginputin.java
@@ -1,7 +1,7 @@
 package Ch1;
 import java.util.Scanner;
 public class Takinginputin {
-    public static void main(Strings[] args){
+    public static void main(String[] args){
     System.out.println("Checking input from user");
     Scanner sc = new Scanner(System.in);
 
