b87de8c7069abe2eb4d044e46b9373d70110e89a
Rohan Maharjan <nahor.uzamaki@gmail.com>
2023-10-23 23:42:28 +0545

    positive and negative

---
+++ src/Ch4/IfElseExample.java
@@ -0,0 +1,15 @@
+package Ch4;
+
+    public class IfElseExample {
+        public static void main(String[] args) {
+            int number = 10;
+    
+            // Check if the number is greater than 0
+            if (number > 0) {
+                System.out.println("The number is positive.");
+            } else {
+                System.out.println("The number is non-positive.");
+            }
+        }
+    }
+
