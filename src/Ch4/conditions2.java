6b907fd790c7c1099fee870ae9b538bc9698ac43
Rohan Maharjan <nahor.uzamaki@gmail.com>
2023-10-24 23:41:59 +0545

    if else for checking grade

---
+++ src/Ch4/conditions2.java
@@ -0,0 +1,18 @@
+package Ch4;
+
+public class conditions2 {
+    public static void main(String[] args) {
+        int examScore = 85;
+
+        if (examScore >= 90) {
+            System.out.println("The student's grade is A.");
+        } else if (examScore >= 80) {
+            System.out.println("The student's grade is B.");
+        } else if (examScore >= 70) {
+            System.out.println("The student's grade is C.");
+        } else {
+            System.out.println("The student's grade is F.");
+        }
+    }
+}
+
