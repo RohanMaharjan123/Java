e0137f9a2dedbd5943f16b12c24d25ae48dcac78
Rohan Maharjan <nahor.uzamaki@gmail.com>
2023-10-27 01:46:43 +0545

    while loop

---
+++ src/Ch5/whileloops.java
@@ -0,0 +1,13 @@
+package Ch5;
+
+public class whileloops {
+    public static void main(String[] args){
+        System.out.println("Using Loops");
+
+        int i = 1;
+        while (i<=3) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
