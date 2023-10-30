555882926da5e57067a7391148e665b014345d41
Rohan Maharjan <nahor.uzamaki@gmail.com>
2023-10-30 23:44:52 +0545

    Ch6 array

---
+++ src/Ch6/arrays.java
@@ -0,0 +1,14 @@
+package Ch6;
+
+public class arrays {
+    public static void main(String[] args){
+        //  store marks of your friends
+        int [] marks = new int[5];
+        marks[0] = 65;
+        marks[1] = 70;
+        marks[2] = 78;
+        marks[3] = 85;
+        marks[4] = 98;
+        System.out.println(marks[0]);
+    }
+}
