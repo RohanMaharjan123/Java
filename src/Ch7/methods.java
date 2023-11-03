64d7f5aa346b44d2a7b98c55d83eaa07dab3eab8
Rohan Maharjan <nahor.uzamaki@gmail.com>
2023-11-03 23:56:08 +0545

    methods

---
+++ src/Ch7/methods.java
@@ -0,0 +1,16 @@
+package Ch7;
+
+public class methods {
+    static int logic(int x, int y){
+        int z;
+        if(x>y){
+            z = x+y;
+        }else{
+            z = (x+y) * 5;
+        }
+        return z;
+    }
+    public static void main(String[] args){
+        
+    }
+}
