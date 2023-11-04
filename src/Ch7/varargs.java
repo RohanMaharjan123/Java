06cd12cdcd8a7b8dcb3ce7f095135ab2caa6e80b
Rohan Maharjan <nahor.uzamaki@gmail.com>
2023-11-04 06:14:14 +0545

    varargs

---
+++ src/Ch7/varargs.java
@@ -0,0 +1,18 @@
+package Ch7;
+
+public class varargs {
+    static int sum(int ...arr){ //(int x, int ...arr)
+        int result = 0; // result = x
+        for(int a: arr){
+            result += a;
+        }
+        return result;
+    }
+    public static void main(String[] args) {
+        // System.out.println("The sum of 1 is: " + sum(1));
+        System.out.println("The sum of 2 and 3 is: " + sum(2,3));
+        System.out.println("The sum of 2, 3 and 4 is: " + sum(2,3,4));
+        System.out.println("The sum of 2, 3, 4 and 5 is: " + sum(2,3,4,5));
+        System.out.println("The sum of 1, 2, 3, 4 and 5 is: " + sum(1,2,3,4,5));
+    }
+}
