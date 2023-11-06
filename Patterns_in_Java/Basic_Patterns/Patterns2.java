6ee9e936935d42b375bc13190cd731db1376ea06
Rohan Maharjan <nahor.uzamaki@gmail.com>
2023-11-07 03:42:34 +0545

    patterns

--- Patterns_in_Java/Basic_Patterns/Patterns2.java
+++ Patterns_in_Java/Basic_Patterns/Patterns2.java
@@ -1,11 +1,11 @@
 package Java.Java.Patterns_in_Java.Basic_Patterns;
 
-public class Patterns1 {
+public class Patterns2 {
     public static void main(String[] args) {
         int n = 5;
 
         // Outer loop
-        for(int i = 1; i <= n; i++){
+        for(int i = n; i >= 1; i--){
             // inner loop
             for(int j = 1; j<=i; j++){
                 System.out.print("*");
--- Patterns_in_Java/Basic_Patterns/Patterns3.java
+++ Patterns_in_Java/Basic_Patterns/Patterns3.java
@@ -1,13 +1,17 @@
 package Java.Java.Patterns_in_Java.Basic_Patterns;
 
-public class Patterns1 {
+public class Patterns3 {
     public static void main(String[] args) {
         int n = 5;
 
         // Outer loop
         for(int i = 1; i <= n; i++){
-            // inner loop
-            for(int j = 1; j<=i; j++){
+            // inner loop for printing spaces
+            for(int j = 1; j<=n-i; j++){
+                System.out.print(" ");
+            }
+            // inner loop for printing star
+            for( int j = 1; j<=i; j++){
                 System.out.print("*");
             }
             System.out.println();
