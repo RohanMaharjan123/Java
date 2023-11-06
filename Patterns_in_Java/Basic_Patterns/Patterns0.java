34c45d872a6f623455723b1328ce187deaa9b3a3
Rohan Maharjan <nahor.uzamaki@gmail.com>
2023-11-07 03:21:02 +0545

    patterns

---
+++ Patterns_in_Java/Basic_Patterns/Patterns0.java
@@ -0,0 +1,21 @@
+package Java.Java.Patterns_in_Java.Basic_Patterns;
+
+public class Patterns0 {
+    public static void main(String[] args) {
+        int n = 4;
+        int m = 5;
+        // Outer Loop
+        for(int i = 1; i<=n; i++){
+            // inner loop
+            for(int j = 1; j<=m; j++){
+                // cell -> (i,j) rows and column
+                if(i == 1 || j == 1 || i == n|| j == m){
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ Patterns_in_Java/Basic_Patterns/Patterns1.java
@@ -0,0 +1,15 @@
+package Java.Java.Patterns_in_Java.Basic_Patterns;
+
+public class Patterns1 {
+    public static void main(String[] args) {
+        int n = 5;
+
+        // Outer loop
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
