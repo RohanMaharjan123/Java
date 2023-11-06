22453d377efcdb8f0cfb457d2f70d426f585425f
Rohan Maharjan <nahor.uzamaki@gmail.com>
2023-11-07 03:56:49 +0545

    patterns

---
+++ Patterns_in_Java/Basic_Patterns/Patterns4.java
@@ -0,0 +1,15 @@
+package Java.Java.Patterns_in_Java.Basic_Patterns;
+
+public class Patterns4 {
+    public static void main(String[] args) {
+        int n = 5;
+        // Outer loop
+        for(int i = 1; i <= n; i++){
+            // inner loop for printing spaces
+            for(int j =1; j<=i; j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+}
