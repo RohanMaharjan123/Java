7171ffa9003c1b515cc8d8b4d357c57b4766ca22
Rohan Maharjan <nahor.uzamaki@gmail.com>
2023-11-17 23:18:37 +0545

    added fun to check negative no

--- src/Ch7/Functions2.java
+++ src/Ch7/Functions2.java
@@ -5,11 +5,16 @@ import java.util.Scanner;
 public class Functions2 {
     public static void printFactorial(int n){
         // loop
+        if(n < 0){
+            System.out.println("Invalid Number");
+            return;
+        }
         int factorial = 1;
         for(int i  = n; i>=1; i--){
             factorial = factorial * i;
         }
         System.out.println(factorial);
+        return;
 
     }
     public static void main(String[] args) {
