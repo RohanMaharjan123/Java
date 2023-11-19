9e1c29cab924e45bd9c6922caa3ada449efb0178
Rohan Maharjan <nahor.uzamaki@gmail.com>
2023-11-19 21:11:46 +0545

    using array in user input

---
+++ src/Ch6/arrays2.java
@@ -0,0 +1,26 @@
+package Ch6;
+
+import java.util.Scanner;
+
+public class arrays2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int numbers[] = new int[size];
+
+        // input
+        for (int i = 0; i < size; i++) {
+            numbers[i] = sc.nextInt();
+        }
+
+        int x = sc.nextInt();
+
+        // output
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] == x) {
+                System.out.println("x found at index : " + i);
+            }
+        }
+        sc.close();
+    }
+}
