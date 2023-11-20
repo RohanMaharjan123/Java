5f52f2e416ff59300b79a5aaf32a9ff76e1c2e46
Rohan Maharjan <nahor.uzamaki@gmail.com>
2023-11-20 23:58:21 +0545

    strings

---
+++ src/Ch3/Strings2.java
@@ -0,0 +1,12 @@
+package Ch3;
+public class Strings2 {
+    public static void main(String[] args) {
+        // String Decleration
+    String name = "Ruchi";
+    String fullname = "Ruchi M";
+    String sentence = "Her name is Ruchi";
+
+    System.out.println(name);
+    }
+
+}
