5aaad196c0a006407701886708840599269262f6
Rohan Maharjan <nahor.uzamaki@gmail.com>
2023-11-21 23:20:22 +0545

    strings

---
+++ src/Ch7/Functions3.java
@@ -0,0 +1,12 @@
+package Ch7;
+
+import java.util.Scanner;
+
+public class Functions3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        System.out.println("Your name is : " + name);
+        sc.close();
+    }
+}
