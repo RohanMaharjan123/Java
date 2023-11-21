248c820f24f7e18190a0f0a8378e175555f3d147
Rohan Maharjan <nahor.uzamaki@gmail.com>
2023-11-21 23:49:45 +0545

    string compare

---
+++ src/Ch3/String6.java
@@ -0,0 +1,13 @@
+package Ch3;
+
+public class String6 {
+    public static void main(String[] args) {
+        // Compare
+        String sentence = "Hi its me I'm the problem it's me.";
+        // substring(beginning index, ending index)
+        String name = sentence.substring(10, sentence.length());
+        System.out.println(name);
+
+        // Strings are immutable
+    }
+}
