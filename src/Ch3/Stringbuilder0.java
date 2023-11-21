78ffeb5e21a7ffaab0d7c2f8ff1b6b087c1ef63f
Rohan Maharjan <nahor.uzamaki@gmail.com>
2023-11-22 00:17:51 +0545

    string builder append

---
+++ src/Ch3/Stringbuilder0.java
@@ -0,0 +1,13 @@
+package Ch3;
+
+public class Stringbuilder0 {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("h");
+        sb.append("e");
+        sb.append("l");
+        sb.append("l");
+        sb.append("o");
+        System.out.println(sb);
+        System.out.println(sb.length());
+    }
+}
