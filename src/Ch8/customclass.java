2240d092965c42d252b19d44054c3c13849bf627
RohanMaharjan123 <nahor.uzamaki@gmail.com>
2023-11-27 23:56:12 +0545

    using custom class

--- src/Ch8/customclass.java
+++ src/Ch8/customclass.java
@@ -4,7 +4,9 @@ class Bird{
     int weight;
     String color;
     public void  printDetails(){
-        System.out.println("Crow is"+ color + "in color");
+        System.out.println("Bird is"+ color + "in color");
+        System.out.println( "Bird " + weight + "kg in weight");
+
     }
 }
 public class customclass {
@@ -12,8 +14,14 @@ public class customclass {
         System.out.println("Custom Class");
 
         Bird crow = new Bird(); // Initializing a Object
+        Bird sparrow = new Bird();
         crow.weight = 15;
         crow.color = "black";
-        System.out.println(crow.weight + " " + crow.color);
+        sparrow.weight = 0.25;
+        sparrow.color = "brown";
+
+        crow.printDetails();
+        sparrow.printDetails();
+
     }
 }
