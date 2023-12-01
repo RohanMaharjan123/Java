a0702c05dea728d502567ad81ed40b2c3a78c3cb
Rohan Maharjan <nahor.uzamaki@gmail.com>
2023-12-01 23:55:52 +0545

    changes in code

--- src/Ch8/qno.java
+++ src/Ch8/qno.java
@@ -1,22 +1,26 @@
 package Ch8;
-
-class Employee{
+class Employee {
     int salary;
     String name;
-    public int getSalary(){
+
+    public int getSalary() {
         return salary;
     }
-    public String getName(){
+
+    public String getName() {
         return name;
     }
-    public void setName(String n){
+
+    public void setName(String n) {
         name = n;
     }
 }
-public class qno{
+
+public class qno {
     public static void main(String[] args) {
-        Employee Josh = new Employee();
-        Josh.setName("Josh Arthur");
-        System.out.println(Josh.getName());
+        Employee hari = new Employee();
+
+        hari.setName("Maha");
+        hari.getName();
     }
 }
