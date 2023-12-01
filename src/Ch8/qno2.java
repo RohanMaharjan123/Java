d3444f9f3a9c47720b9fe93de176baa5bfdcc7d3
Rohan Maharjan <nahor.uzamaki@gmail.com>
2023-12-02 01:22:46 +0545

    area & perimeter of a rectangle

---
+++ src/Ch8/qno2.java
@@ -0,0 +1,21 @@
+package Ch8;
+
+class Rectangle{
+    int length,breadth;
+
+    public int area(){
+        return length*breadth;
+    }
+    public int perimeter(){
+        return 2*(length + breadth);
+    }
+}
+public class qno2{
+    public static void main(String[] args) {
+        Rectangle rc = new Rectangle();
+        rc.length = 5;
+        rc.breadth =6;
+        System.out.println(rc.area());
+        System.out.println(rc.perimeter());
+    }
+}
