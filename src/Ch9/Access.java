975d8898af2dc60fd54bb7c53d9b8e351ad53da6
Rohan Maharjan <nahor.uzamaki@gmail.com>
2023-12-02 23:38:27 +0545

    Access modifier

---
+++ src/Ch9/Access.java
@@ -0,0 +1,29 @@
+package Ch9;
+
+class MyEmployee{
+    private int id;
+    private String name;
+
+    public String getName(){
+        return name;
+    }
+    public void setName(String n){
+        name = n;
+    }
+    public void setId(int i){
+        id = i;
+    }
+    public int getId(){
+        return id;
+    }
+
+}
+public class Access {
+    public static void main(String[] args) {
+        MyEmployee hari = new MyEmployee();
+        // hari.id = 100;
+        // hari.name = "Hari bansha Acharaya"; --> provides error due to private access modifier
+        hari.setName("Hari bansha Acharaya");
+        System.out.println(hari.getName());
+    }
+}
