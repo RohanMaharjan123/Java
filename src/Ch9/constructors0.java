42e236a40f97f93d4f49642a53e9a9c3433d19a8
Rohan Maharjan <nahor.uzamaki@gmail.com>
2023-12-04 00:01:49 +0545

    passing value from method

---
+++ src/Ch9/constructors0.java
@@ -0,0 +1,37 @@
+package Ch9;
+
+class MyBIEmployee {
+    private int id;
+    private String name;
+
+    public MyBIEmployee(String myName, int myId) {
+        id = myId;
+        name = myName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String n) {
+        name = n;
+    }
+
+    public void setId(int i) {
+        id = i;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
+
+public class constructors0 {
+    public static void main(String[] args) {
+        MyBIEmployee homas = new MyBIEmployee("Cicero", 2);
+        // homas.setName("Homasiuous");
+        // homas.setId(2);
+        System.out.println(homas.getName());
+        System.out.println(homas.getId());
+    }
+}
