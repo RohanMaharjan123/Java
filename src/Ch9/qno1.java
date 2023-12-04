e08331f42ef8009623a2dbff7992e154f48ac68c
Rohan Maharjan <nahor.uzamaki@gmail.com>
2023-12-05 01:49:08 +0545

    Area and perimeter using constructor

---
+++ src/Ch9/qno1.java
@@ -0,0 +1,37 @@
+package Ch9;
+
+class Rectangle{
+    private int length;
+    private int breadth;
+
+    public Rectangle(){
+        this.length = 2;
+        this.breadth = 3;
+    }
+    public Rectangle(int length, int breadth){
+        this.length = length;
+        this.breadth = breadth;
+    }
+    public int getLength(){
+        return length;
+    }
+    public int getBreadth(){
+        return breadth;
+    }
+    public int area(){
+        return length * breadth;
+    }
+    public int perimeter(){
+        return 2 * (length + breadth);
+    }
+}
+public class qno1 {
+    public static void main(String[] args) {
+        Rectangle r = new Rectangle(12,15);
+        System.out.println(r.getLength());
+        System.out.println(r.getBreadth());
+
+        System.out.println("Area of the rectangle is: " + r.area());
+        System.out.println("Perimeter of the rectangle is: " + r.perimeter());
+    }
+}
