76b0c55eac98597eb7df836698ad4f75c7fdd768
Rohan Maharjan <nahor.uzamaki@gmail.com>
2023-12-09 00:08:09 +0545

    multi value in constructor

--- src/Ch10/constructors_in_inheritance.java
+++ src/Ch10/constructors_in_inheritance.java
@@ -4,29 +4,23 @@ class Base0{
     Base0(){
         System.out.println("Hi, constructor here");
     }
-    public int x;
-    public int getX(){
-        return x;
-    }
-    public void setX(int x){
-        this.x = x;
+    Base0(int a){
+        System.out.println("Hi, constructor here with value a" + a);
     }
 }
 class Derived0 extends Base0{
     Derived0(){
+        // super(0);
         System.out.println("Hi there derived class constructor here");
     }
-    public int y;
-    public int getY(){
-        return y;
-    }
-    public void setY(int y){
-        this.y = y;
-    }
+    // Derived0(int x, int y){
+    //     System.out.println("Hi there derived class constructor here");
+    // }
 }
 public class constructors_in_inheritance {
     public static void main(String[] args) {
         // Base0 b = new Base0();
+        
         Derived0 d = new Derived0();
     }
 }
