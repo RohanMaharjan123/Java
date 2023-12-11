c07a715f6980d06b70439b6b67892c780f43d5cf
Rohan Maharjan <nahor.uzamaki@gmail.com>
2023-12-11 23:56:25 +0545

    method overriding

---
+++ src/Ch10/method_overriding.java
@@ -0,0 +1,15 @@
+package Ch10;
+
+class A{
+    public int hi(){
+        return 4;
+    }
+    public void meth1(){
+        
+    }
+}
+public class method_overriding {
+    public static void main(String[] args) {
+        
+    }
+}
