04b6a98d239a1740a6df46efd83afdcd1b228f3f
Rohan Maharjan <nahor.uzamaki@gmail.com>
2023-12-12 23:11:56 +0545

    overridding

--- src/Ch10/method_overriding.java
+++ src/Ch10/method_overriding.java
@@ -1,15 +1,29 @@
 package Ch10;
 
 class A{
+    public int a;
     public int hi(){
         return 4;
     }
     public void meth1(){
-        
+        System.out.println("2nd method of A");
+    }
+}
+class B extends A{
+    @Override
+    public void meth1(){
+        System.out.println("2nd method of B");
+    }
+    public void meth2(){
+        System.out.println("3rd method of B");
     }
 }
 public class method_overriding {
     public static void main(String[] args) {
-        
+        A a = new A();
+        a.meth1();
+
+        B b = new B();
+        b.meth1();
     }
 }
