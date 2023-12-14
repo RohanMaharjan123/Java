4e6dff88aec7a3a3f9a159e687ede5a27e9e38c6
Rohan Maharjan <nahor.uzamaki@gmail.com>
2023-12-14 23:54:47 +0545

    restructuring the guessing number

---
+++ src/Ch09/Game0.java
@@ -0,0 +1,18 @@
+package Ch09;
+
+class Game{
+    public int number;
+    public int noOfGuesses;
+    void Game(int n){
+        this.number = n;
+    }
+    void takeUserInput(){
+
+    }
+    boolean isCorrectNumber(){
+
+    }
+}
+public class Game0 {
+    
+}
