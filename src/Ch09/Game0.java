7ea7bf74d7fe48ec31c1bc81cfb4b46ecad9a266
Rohan Maharjan <nahor.uzamaki@gmail.com>
2023-12-15 23:50:29 +0545

    adding classes and methods

--- src/Ch09/Game0.java
+++ src/Ch09/Game0.java
@@ -1,18 +1,34 @@
 package Ch09;
-
+import java.util.Random;
+import java.util.Scanner;
 class Game{
     public int number;
+    public int inputNumber;
     public int noOfGuesses;
-    void Game(int n){
-        this.number = n;
+    public int getNoOfGuesses(){
+        return noOfGuesses;
+    }
+    public void setNoOfGuesses(int noOfGuesses){
+        this.noOfGuesses = noOfGuesses;
+    }
+
+    void Game(){
+        Random rand = new Random();
+        this.number = rand.nextInt(100);
     }
     void takeUserInput(){
-
+        Scanner sc = new Scanner(System.in);
+        inputNumber = sc.nextInt();
     }
-    boolean isCorrectNumber(){
-
+    boolean isCorrectNumber(int num){
+        if(num == number){
+            return true;
+        }
+        return false;
     }
 }
 public class Game0 {
-    
+    public static void main(String[] args) {
+        Game g = new Game();
+    }
 }
