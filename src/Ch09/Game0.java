03d6c0e055fb9be69e4da287f79b056547fad136
Rohan Maharjan <nahor.uzamaki@gmail.com>
2023-12-16 23:55:15 +0545

    progress

--- src/Ch09/Game0.java
+++ src/Ch09/Game0.java
@@ -12,17 +12,23 @@ class Game{
         this.noOfGuesses = noOfGuesses;
     }
 
-    void Game(){
+    Game(){
         Random rand = new Random();
         this.number = rand.nextInt(100);
     }
     void takeUserInput(){
+        System.out.println("Guess the number");
         Scanner sc = new Scanner(System.in);
         inputNumber = sc.nextInt();
+        sc.close();
     }
-    boolean isCorrectNumber(int num){
-        if(num == number){
+    boolean isCorrectNumber(){
+        if(inputNumber == number){
             return true;
+        }else if(inputNumber < number){
+            System.out.println("Too Low.....");
+        }else if(inputNumber > number){
+            System.out.println("Too High.....");
         }
         return false;
     }
@@ -30,5 +36,8 @@ class Game{
 public class Game0 {
     public static void main(String[] args) {
         Game g = new Game();
+        g.takeUserInput();
+        boolean b = g.isCorrectNumber();
+        System.out.println(b);
     }
 }
