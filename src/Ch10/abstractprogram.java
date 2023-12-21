78537e308197e62b8da53ea117baee94a5663fd3
Rohan Maharjan <nahor.uzamaki@gmail.com>
2023-12-21 23:51:56 +0545

    abstract class in java

---
+++ src/Ch10/abstractprogram.java
@@ -0,0 +1,16 @@
+package Ch10;
+
+abstract class Base0{
+    public Base1(){
+        System.out.println("constructor of Base0");
+    }
+    public void sayHello(){
+        System.out.println("Hello");
+    }
+    abstract public void greet();
+}
+public class abstractprogram {
+    public static void main(String[] args) {
+        
+    }
+}
