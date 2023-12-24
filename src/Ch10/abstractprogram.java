5ae7d742905c51e1623df97930a95e63bf696a5f
Rohan Maharjan <nahor.uzamaki@gmail.com>
2023-12-24 23:46:50 +0545

    using abstract method and class

--- src/Ch10/abstractprogram.java
+++ src/Ch10/abstractprogram.java
@@ -1,7 +1,7 @@
 package Ch10;
 
-abstract class Base0{
-    public Base1(){
+abstract class Parent{
+    public Parent0(){
         System.out.println("constructor of Base0");
     }
     public void sayHello(){
@@ -9,8 +9,20 @@ abstract class Base0{
     }
     abstract public void greet();
 }
+class Child extends Parent{
+    @Override
+    public void greet(){
+        System.out.println("Good morning");
+    }
+} 
+abstract class Child0 extends Parent{
+    public void th(){
+        System.out.println("I am good");
+    }
+}
 public class abstractprogram {
     public static void main(String[] args) {
-        
+        Child c = new Child();
+        // Child0 c0 = new Child0(); -- error 
     }
 }
