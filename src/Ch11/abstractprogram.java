290f9eb0e0cbeb2f1d8f3e520ee3f449afd993ad
Rohan Maharjan <nahor.uzamaki@gmail.com>
2023-12-28 23:59:49 +0545

    abstract class and interface

---
+++ src/Ch11/abstractprogram.java
@@ -0,0 +1,28 @@
+package Ch11;
+
+abstract class Parent{
+    public Parent(){
+        System.out.println("constructor of Base0");
+    }
+    public void sayHello(){
+        System.out.println("Hello");
+    }
+    abstract public void greet();
+}
+class Child extends Parent{
+    @Override
+    public void greet(){
+        System.out.println("Good morning");
+    }
+} 
+abstract class Child0 extends Parent{
+    public void th(){
+        System.out.println("I am good");
+    }
+}
+public class abstractprogram {
+    public static void main(String[] args) {
+        Child c = new Child();
+        // Child0 c0 = new Child0(); -- error 
+    }
+}
