a570ff0b32679c8afcc2549791a60b78503bde79
Rohan Maharjan <nahor.uzamaki@gmail.com>
2024-01-03 23:43:46 +0545

    minor change

--- src/Ch11/interface0.java
+++ src/Ch11/interface0.java
@@ -1,5 +1,24 @@
 package Ch11;
 
+interface Bicycle{
+    void applyBrake(int decrement);
+    void speedUp(int increment);
+}
+class AvonCycle implements Bicycle{
+    void blowHorn(){
+        System.out.println("sound of horn");
+    }
+    public void applyBrake(int decrement){
+        System.out.println("Applying Brake");
+    }
+    public void speedUp(int increment){
+        System.out.println("Applying Speed up");
+    }
+}
 public class interface0 {
-    
+    public static void main(String[] args) {
+        AvonCycle cycle0 = new AvonCycle();
+        cycle0.applyBrake(1);
+        // You cannot modify the properties in Interfaces as they are final
+    }
 }
